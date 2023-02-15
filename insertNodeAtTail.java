a02d953b4f0e979f09df7f179c20200d6bf0315a
Roshiniii <94703050+Roshiniii@users.noreply.github.com>
2023-02-15 09:51:54 +0530

    Create insertNodeAtTail.java

---
+++ insertNodeAtTail.java
@@ -0,0 +1,27 @@
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode current = head;
+        SinglyLinkedListNode new_node = new SinglyLinkedListNode(data);
+        
+        if(head == null){
+            head = new_node;
+            return head;
+        }
+        
+        while(current.next != null){
+            current= current.next;
+        }
+        
+        current.next = new_node;
+        return head;
+    }
