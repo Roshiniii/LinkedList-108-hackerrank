57730457287fecea275b405ea0c5acd21b6d3bed
Roshiniii <94703050+Roshiniii@users.noreply.github.com>
2023-02-15 09:56:53 +0530

    Create hasCycle.java

---
+++ hasCycle.java
@@ -0,0 +1,27 @@
+    // Complete the hasCycle function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static boolean hasCycle(SinglyLinkedListNode head) {
+       if(head == null || head.next == null) return false;
+        
+        SinglyLinkedListNode fast = head;
+        SinglyLinkedListNode slow = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(fast == slow)
+                return true;
+        }
+        return false;
+
+    }
